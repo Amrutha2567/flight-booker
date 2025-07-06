8764f4013245c0b89bf4242e8f72fd099a77a4c4
Amrutha <102677089+Amrutha2567@users.noreply.github.com>
2025-07-06 11:01:50 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FlightBooker from './components/FlightBooker';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <FlightBooker />
+    </div>
+  );
+}
+
+export default App;
